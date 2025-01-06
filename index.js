9a534eb51d393e35f46c309ed28bded3ba3da8e8
Anjudimpy <anjukr55555@gmail.com>
2025-01-06 19:59:30 +0530

    second

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import ProductController from './src/controllers/product.controller.js';
+
+const server = express();
+
+//Create an instance of ProductController
+const productController =  new ProductController();
+server.get('/', productController.getProducts);
+
+server.use(express.static('src/views'));
+
+server.listen(3400);
