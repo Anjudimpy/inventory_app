3f5d8097f2976f77eb759302e3c8fc08417e6118
Anjudimpy <anjukr55555@gmail.com>
2025-01-21 14:45:10 +0530

    secure all page

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ import ejsLayouts from 'express-ejs-layouts';
 import validationRequest from './src/middlewares/validation.middleware.js';
 import { uploadFile } from './src/middlewares/file-upload.middleware.js';
 import UserController from './src/controllers/user.controller.js';
+import session from 'express-session';
+import { auth } from './src/middlewares/auth.middleware.js';
 
 const server = express();
 
 //parse from data
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
+
+server.use(
+    session({
+      secret:'SecretKey',
+      resave:false,
+      saveUninitialized: true,
+      cookie:{secure: false},
+    })
+)
 
 // setup view engine setting
 server.set('view engine', 'ejs');
@@ -21,14 +32,17 @@ server.use(ejsLayouts);
 //Create an instance of ProductController
 const productController =  new ProductController();
 const userController = new UserController();
-server.get('/', productController.getProducts);
-server.get('/new', productController.getNewForm);
-server.post('/', uploadFile.single('imageUrl'), validationRequest, productController.addNewProduct);
-server.get('/update-product/:id', productController.getUpdateProduct);
-server.post('/update-product', productController.postUpdateProduct);
-server.post('/delete-product/:id', productController.deleteProduct);
-server.post('/search', productController.search );
+server.get('/', auth, productController.getProducts);
+server.get('/new', auth, productController.getNewForm);
+server.post('/', auth, uploadFile.single('imageUrl'), validationRequest, productController.addNewProduct);
+server.get('/update-product/:id',auth, productController.getUpdateProduct);
+server.post('/update-product', auth, productController.postUpdateProduct);
+server.post('/delete-product/:id', auth, productController.deleteProduct);
+server.post('/search',auth, productController.search );
 server.get('/register', userController.getRegister);
+server.post('/register', userController.postRegister)
+server.get('/login', userController.getLogin);
+server.post('/login', userController.postLogin)
 server.use(express.static('src/views'));
 
 server.listen(3400);
